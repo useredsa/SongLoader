53160874500d38e19d11c91cb47cc89f67f4d89e
useredsa <emilio.dominguezs@um.es>
2020-12-05 11:45:56 +0100

    Format Code to style guide

--- src/main/java/umu/tds/componente/package-info.java
+++ src/main/java/umu/tds/componente/package-info.java
@@ -1,9 +1,13 @@
 //
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2017.10.26 a las 12:42:42 AM CEST 
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de
+// enlace (JAXB) XML v2.2.8-b130911.1802
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el
+// esquema de origen.
+// Generado el: 2017.10.26 a las 12:42:42 AM CEST
 //
 
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.tds.es/canciones", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+@javax.xml.bind.annotation.XmlSchema(
+    namespace = "http://www.tds.es/canciones",
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package umu.tds.componente;
